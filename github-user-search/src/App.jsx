5007d37f84b7dcfd68f4e196619a660b0b6b0b03
lone_ndegwa <ndegwaeddie67@gmail.com>
2025-08-02 19:10:03 +0300

    API

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>GitHub User Search</h1>
+      <p>Start typing a GitHub username to search.</p>
+    </div>
+  );
+}
+
+export default App;
