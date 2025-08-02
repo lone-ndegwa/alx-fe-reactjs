fd6b9ed4013441133a75da34076dfd28af9c6714
lone_ndegwa <ndegwaeddie67@gmail.com>
2025-08-02 19:37:22 +0300

    API

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -37,7 +37,7 @@ const Search = () => {
       </form>
 
       {loading && <p>Loading...</p>}
-      {error && <p>Looks like we can't find the user.</p>}
+      {error && <p>Looks like we cant find the user</p>}
       {user && (
         <div style={{ marginTop: '20px' }}>
           <img src={user.avatar_url} alt={user.login} width="100" />
@@ -54,4 +54,3 @@ const Search = () => {
 };
 
 export default Search;
-    
