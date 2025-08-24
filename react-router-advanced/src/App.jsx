64bd83a8053cda8a49d096a9881c768b5529d629
lone_ndegwa <ndegwaeddie67@gmail.com>
2025-08-24 18:49:22 +0300

    React Router

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,20 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
-import RecipeDetail from "./components/RecipeDetail";
-
-// Temporary placeholder for the blog detail route
-function BlogDetail() {
-  return <h1>Blog Detail Page</h1>;
-}
+import BlogPost from "./components/BlogPost";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Home page */}
         <Route path="/" element={<HomePage />} />
-        <Route path="/recipe/:id" element={<RecipeDetail />} />
-        <Route path="/blog/:id" element={<BlogDetail />} /> {/* Required for check */}
+        
+        {/* Dynamic blog route */}
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </Router>
   );
